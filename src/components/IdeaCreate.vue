7c7d6408d51a9091fcb5562589749329d070b55e
Hermann Manuel <hermann.manuel@hotmail.com>
2021-01-11 10:53:02 +0100

    Création des components

---
+++ src/components/IdeaCreate.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <form @submit.prevent="create"> <!-- Eviter le rechargement complet de la page avec ".prevent"-->
+            <label for="idea-title">Idea's title</label><br>
+            <input type="text" id="idea-title" v-model="title" /> <!-- v-model permet de récupérer la valeur saisie -->
+            <button type="submit">Create</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                title: ""
+            }
+        },
+        methods: {
+            create() {
+                console.log(this.title)
+                this.title = "" //Après la soumission du formulaire, on reintialise le formulaire à un champ vide
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Ideas.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Ideas!</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
